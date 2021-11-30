f4b88bac09f9236561537e121c3ddfe969d3f4ea
devmed <medmarwane@gmail.com>
2021-11-30 20:48:53 +0100

    Preparing our Files For Go Live part:2

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,25 @@
 const currentTask = process.env.npm_lifecycle_event
 const path = require("path")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CssMinimazerPlugin = require("css-minimizer-webpack-plugin")
 
 const postCSSPlugins = [require("postcss-import"), require("postcss-mixins"), require("postcss-simple-vars"), require("postcss-nested"), require("autoprefixer")]
+
+let cssConfig = {
+  test: /\.css$/i,
+  use: ["css-loader", { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]
+}
 
 let config = {
   entry: "./app/assets/scripts/App.js",
   module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader", { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]
-      }
-    ]
+    rules: [cssConfig]
   }
 }
 
 if (currentTask == "dev") {
+  cssConfig.use.unshift("style-loader")
   config.output = {
     filename: "bundled.js",
     path: path.resolve(__dirname, "app")
@@ -30,11 +34,19 @@ if (currentTask == "dev") {
 }
 
 if (currentTask == "build") {
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader)
   config.output = {
-    filename: "bundled.js",
+    filename: "[name].[chunkhash].js",
+    chunkFilename: "[name].[chunkhash].js",
     path: path.resolve(__dirname, "dist")
   }
   config.mode = "production"
+  config.optimization = {
+    splitChunks: { chunks: "all" },
+    minimize: true,
+    minimizer: [`...`, new CssMinimazerPlugin()]
+  }
+  config.plugins = [new CleanWebpackPlugin(), new MiniCssExtractPlugin({ filename: "styles.[chunkhash].css" })]
 }
 
 module.exports = config
