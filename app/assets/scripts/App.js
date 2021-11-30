be1d6e42d4bd1f610a32edebe6d3fe2db1e29405
devmed <medmarwane@gmail.com>
2021-11-30 19:13:32 +0100

    Completed Reveal On Scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,7 +3,8 @@ import MobileMenu from "./modules/MobileMenu"
 import RevealOnScroll from "./modules/RevealOnScroll"
 
 let mobileMenue = new MobileMenu()
-let revealOnScroll = new RevealOnScroll()
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 85)
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 75)
 
 if (module.hot) {
   module.hot.accept()
