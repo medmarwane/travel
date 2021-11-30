527691133d8b5b0c10ddf208f9945a5436e4e55c
devmed <medmarwane@gmail.com>
2021-11-30 20:40:07 +0100

    Preparing our Files For Go Live part:1

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,10 @@
+const currentTask = process.env.npm_lifecycle_event
 const path = require("path")
+
 const postCSSPlugins = [require("postcss-import"), require("postcss-mixins"), require("postcss-simple-vars"), require("postcss-nested"), require("autoprefixer")]
 
-module.exports = {
+let config = {
   entry: "./app/assets/scripts/App.js",
-  output: {
-    filename: "bundled.js",
-    path: path.resolve(__dirname, "app")
-  },
-  devServer: {
-    static: path.join(__dirname, "app"),
-    hot: true,
-    port: 3000,
-    host: "0.0.0.0"
-  },
-  mode: "development",
   module: {
     rules: [
       {
@@ -23,3 +14,27 @@ module.exports = {
     ]
   }
 }
+
+if (currentTask == "dev") {
+  config.output = {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app")
+  }
+  config.devServer = {
+    static: path.join(__dirname, "app"),
+    hot: true,
+    port: 3000,
+    host: "0.0.0.0"
+  }
+  config.mode = "development"
+}
+
+if (currentTask == "build") {
+  config.output = {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "dist")
+  }
+  config.mode = "production"
+}
+
+module.exports = config
