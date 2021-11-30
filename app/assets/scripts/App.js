8f64301f2dac173977a75494034ee037cd014a25
devmed <medmarwane@gmail.com>
2021-11-30 19:20:43 +0100

    Desktop Sticky Header part:1

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,11 @@
 import "../styles/styles.css"
 import MobileMenu from "./modules/MobileMenu"
 import RevealOnScroll from "./modules/RevealOnScroll"
+import StickyHeader from "./modules/StickyHeader"
 
+let stickyHeader = new StickyHeader()
 let mobileMenue = new MobileMenu()
+
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 85)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 75)
 
